f67c6f89290aacca53fe9acc40494167a6c8c8e8
Marisa Neamonitis <m.neamonitis@gmail.com>
2016-09-14 12:19:42 -0400

    Week3HW

--- sketch.js
+++ sketch.js
@@ -7,8 +7,12 @@
   var pizRightY = 100
   
   var pepColor = 'rgb(100%,0%,10%)'; 
-  var pepSize = 70
+  var pepSize = 67
   
+  var offsetX = 0;
+  var offsetY = 0;
+  
+  var backgroundCounter = 0;
 
 
 console.log(pizRightY)
@@ -17,13 +21,27 @@ console.log(pizRightY)
 function setup() {
   createCanvas(500,500)
 
-   background(0, 0, 255);
+   
 }
 
 
 
 function draw() {
 
+ // background(0, 0, 255);
+ if (backgroundCounter == 0) {
+    background('#fae');
+  } else if (backgroundCounter == 1) {
+    background('rgb(0,255,100)');
+  } else if (backgroundCounter == 2) {
+    background(0, 200, 255);
+  } else {
+    backgroundCounter = 0;
+  }
+
+  
+  offsetX = map(mouseX, 0, width, -10,10);
+  offsetY = map(mouseY, 0, height, -10,10);
 
   //pizza
   fill(255, 204, 0);
@@ -39,12 +57,18 @@ function draw() {
   
   //perperoni
   fill(pepColor);
-  ellipse(275,187,pepSize,pepSize);
-  arc(200, 200, pepSize, pepSize,PI, HALF_PI+QUARTER_PI, CHORD);
-  arc(258, 300, pepSize, pepSize, QUARTER_PI, 0, CHORD);
+  //ellipse(275,187,pepSize,pepSize);
+  ellipse (275 + offsetX,187 + offsetY,pepSize,pepSize)
+  //arc(200, 200, pepSize, pepSize,PI, HALF_PI+QUARTER_PI, CHORD);
+  arc(200 + offsetX, 200 + offsetY, pepSize, pepSize, PI, HALF_PI+QUARTER_PI, CHORD)
+  arc(258 + offsetX, 300 + offsetY, pepSize, pepSize, QUARTER_PI, 0, CHORD);
   
+  
+  
+}
 
-  
-  
+
+function mouseReleased() {
+ backgroundCounter = backgroundCounter + 1;
 }
   
